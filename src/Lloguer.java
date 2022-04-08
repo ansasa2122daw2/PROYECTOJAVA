153adb24d0e1b2fa787fc4f058efa6d91e298a86
ansasa2122daw2 <ansasa2122daw2@protonmail.com>
2022-04-08 17:31:32 +0200

    Commit lloguer manteniment comienzo

--- src/Lloguer.java
+++ src/Lloguer.java
@@ -1,2 +1,86 @@
+import java.sql.*;
+import java.util.Scanner;
+
 public class Lloguer {
+    String dni;
+    String matricula;
+    String dias;
+    int preu_per_dias;
+    String lloc_devolucio;
+    boolean retorn_disposit_ple;
+    String tipus_asseguranca;
+
+    private static final Lloguer loguer = new Lloguer();
+    Connection conexion = (new Conexion()).conexion;
+
+    public Lloguer() {
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getDias() {
+        return dias;
+    }
+
+    public void setDias(String dias) {
+        this.dias = dias;
+    }
+
+    public int getPreu_per_dias() {
+        return preu_per_dias;
+    }
+
+    public void setPreu_per_dias(int preu_per_dias) {
+        this.preu_per_dias = preu_per_dias;
+    }
+
+    public String getLloc_devolucio() {
+        return lloc_devolucio;
+    }
+
+    public void setLloc_devolucio(String lloc_devolucio) {
+        this.lloc_devolucio = lloc_devolucio;
+    }
+
+    public boolean isRetorn_disposit_ple() {
+        return retorn_disposit_ple;
+    }
+
+    public void setRetorn_disposit_ple(boolean retorn_disposit_ple) {
+        this.retorn_disposit_ple = retorn_disposit_ple;
+    }
+
+    public String getTipus_asseguranca() {
+        return tipus_asseguranca;
+    }
+
+    public void setTipus_asseguranca(String tipus_asseguranca) {
+        this.tipus_asseguranca = tipus_asseguranca;
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    //METODOS
+    //TODO: LISTAR
 }
+
--- src/Manteniment.java
+++ src/Manteniment.java
@@ -1,2 +1,61 @@
+import java.sql.*;
+import java.util.Scanner;
+
+
 public class Manteniment {
+    String dni_mecanic;
+    String matricula;
+    Date data_inici;
+    Date data_fi;
+
+    private static final Manteniment manteniment = new Manteniment();
+    Connection conexion = (new Conexion()).conexion;
+
+    public Manteniment() {
+    }
+
+    public String getDni_mecanic() {
+        return dni_mecanic;
+    }
+
+    public void setDni_mecanic(String dni_mecanic) {
+        this.dni_mecanic = dni_mecanic;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Date getData_inici() {
+        return data_inici;
+    }
+
+    public void setData_inici(Date data_inici) {
+        this.data_inici = data_inici;
+    }
+
+    public Date getData_fi() {
+        return data_fi;
+    }
+
+    public void setData_fi(Date data_fi) {
+        this.data_fi = data_fi;
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    //METODOS
+
+    //TODO: LISTAR
 }
+
